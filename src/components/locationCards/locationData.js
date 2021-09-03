2caa26c5aba1c15fe8010efa4921467b25f47c53
Stetson Harding <stetson2001@msn.com>
2021-09-02 17:48:50 -0700

    added new image for evergreen way.

--- src/components/locationCards/locationData.js
+++ src/components/locationCards/locationData.js
@@ -2,7 +2,7 @@ import Clearview from "../../images/Clearview.PNG";
 import Cathcart from "../../images/Cathcart.PNG";
 import LakeForestPark from "../../images/LakeForestPark.PNG";
 import Stanwood from "../../images/Stanwood.PNG";
-import EvergreenWayOpen from "../../images/EvergreenWayOpen.jpg";
+import EvergreenWayOpen from "../../images/EvergreenWayStore.jpg";
 
 export const locationData = [
   {
@@ -16,7 +16,7 @@ export const locationData = [
     address: "4808 Evergreen Way",
     stateAndZip: "Everett, WA 98203",
     directions:
-      "https://www.google.com/maps/dir/48.0313344,-122.1296128/4808+Evergreen+Way,+Everett,+WA+98203/@47.9926549,-122.2052678,13z/data=!3m2!4b1!5s0x5490088c964031df:0x267cb74ba90c61cb!4m9!4m8!1m1!4e1!1m5!1m1!1s0x54900088c8c453cf:0xf70ff920a7aa5b99!2m2!1d-122.2137449!2d47.9539065",
+      "https://www.google.com/maps/place/The+Kushery+Everett+Evergreen+Way/@47.9539101,-122.2159336,17z/data=!3m1!4b1!4m5!3m4!1s0x549001f2b162925d:0xbbeef621ecfeb9e6!8m2!3d47.9538939!4d-122.2135758",
     menu: "https://www.iheartjane.com/embed/stores/3585",
   },
   {
@@ -30,7 +30,7 @@ export const locationData = [
     address: "18026 WA-9 B,",
     stateAndZip: "Snohomish, WA 98296",
     directions:
-      "https://www.google.com/maps/dir//kushery+clearview/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x5490088cbde3e777:0x2bcec041a50b8fcb?sa=X&ved=2ahUKEwjbqoKq6ObsAhWCFzQIHaobCBgQ9RcwC3oECBQQBA",
+      "https://www.google.com/maps/place/The+Kushery+Clearview/@47.8333666,-122.1290446,17z/data=!3m2!4b1!5s0x5490088c964031df:0x267cb74ba90c61cb!4m5!3m4!1s0x5490088cbde3e777:0x2bcec041a50b8fcb!8m2!3d47.833363!4d-122.1268559",
     menu: "https://www.iheartjane.com/embed/stores/2617",
   },
   {
@@ -45,7 +45,7 @@ export const locationData = [
     address: "5626 134th Pl SE B,",
     stateAndZip: " Everett, WA 98208",
     directions:
-      "https://www.google.com/maps/dir//the+kushery+cathcart/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x549007b1c9bbc243:0xa9d8ad49b63f28d4?sa=X&ved=2ahUKEwiSt8OrnunsAhXzIDQIHROqA4YQ9RcwAHoECAMQBA",
+      "https://www.google.com/maps/place/The+Kushery+Cathcart/@47.8334225,-122.1618751,13z/data=!3m1!5s0x549007b1cbc174fb:0xe90413d8f7c91d0!4m9!1m2!2m1!1sthe+kushery+cathcart!3m5!1s0x549007b1c9bbc243:0xa9d8ad49b63f28d4!8m2!3d47.8765277!4d-122.1563823!15sChR0aGUga3VzaGVyeSBjYXRoY2FydCIDiAEBWhYiFHRoZSBrdXNoZXJ5IGNhdGhjYXJ0kgEOY2FubmFiaXNfc3RvcmU",
     menu: "https://www.iheartjane.com/embed/stores/2618",
   },
   {
@@ -60,7 +60,7 @@ export const locationData = [
     address: "19258 15th Ave NE #B,",
     stateAndZip: "Lake Forest Park, WA 98155",
     directions:
-      "https://www.google.com/maps/dir//kushery+lfp/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x54901043c7444211:0x2dc611155f2ba4da?sa=X&ved=2ahUKEwjowPyKn-nsAhWhIzQIHW8vDGgQ9RcwC3oECBAQBA",
+      "https://www.google.com/maps/place/The+Kushery+Lake+Forest+Park/@47.7698057,-122.3134974,19.98z/data=!3m1!5s0x54901043c6551dad:0xc32eb44f9f91580c!4m5!3m4!1s0x54901043c7444211:0x2dc611155f2ba4da!8m2!3d47.7698544!4d-122.3132431",
     menu: "https://www.iheartjane.com/embed/stores/2620",
   },
   {
@@ -74,7 +74,7 @@ export const locationData = [
     address: "27206 88th Ave NW,",
     stateAndZip: "Stanwood, WA 98292",
     directions:
-      "https://www.google.com/maps/dir//stanwood+kushery/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x54855d87b08c86fd:0x4cce6e428a0c2d25?sa=X&ved=2ahUKEwjz3OWyoOnsAhW_CTQIHSMMBAwQ9RcwC3oECBMQBA",
+      "https://www.google.com/maps/place/The+Kushery+Stanwood/@48.2432397,-122.3539673,17z/data=!3m2!4b1!5s0x54855c68dd64a757:0xe7a789a66502003!4m5!3m4!1s0x54855d87b08c86fd:0x4cce6e428a0c2d25!8m2!3d48.2432079!4d-122.3518601",
     menu: "https://www.iheartjane.com/embed/stores/2619",
   },
 ];
